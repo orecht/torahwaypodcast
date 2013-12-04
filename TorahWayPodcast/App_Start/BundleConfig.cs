1afdf63b3ed172ffd8b9388e363c19d9486ee6e9
Oren Recht <oren_recht@hotmail.com>
2013-12-04 23:10:02 +0000

    Removed unused bundles and make site work

--- TorahWayPodcast/App_Start/BundleConfig.cs
+++ TorahWayPodcast/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace TorahWayPodcast
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            /*
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -17,9 +18,11 @@ namespace TorahWayPodcast
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
+             * */
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            /*
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -38,6 +41,7 @@ namespace TorahWayPodcast
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+             * */
         }
     }
 }
