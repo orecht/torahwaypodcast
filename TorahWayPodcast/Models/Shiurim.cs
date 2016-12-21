224c62fed6eeb9b4c90cfd1744d5dbc8e5258f8a
Oren Recht <oren.recht@pkrtech.com>
2016-12-21 17:05:30 +0000

    cleanup shiurim

--- TorahWayPodcast/Models/Shiurim.cs
+++ TorahWayPodcast/Models/Shiurim.cs
@@ -6,21 +6,5 @@ using System.Web;
 namespace TorahWayPodcast.Models
 {
     public class Shiurim : List<Shiur>
-    {
-        /*
-        static private Shiurim f_instance = new Shiurim();
-        static public Shiurim Instance
-        {
-            get
-            {
-                return f_instance;
-            }
-        }
-
-        private Shiurim()
-            : base()
-        {
-        }
-         * */
-    }
+    { }
 }
