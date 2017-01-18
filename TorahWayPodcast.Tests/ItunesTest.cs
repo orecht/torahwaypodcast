4f7eb21ad64bcab02c9e0f55f084155efa4e7a82
Oren Recht <oren.recht@pkrtech.com>
2017-01-18 13:24:42 +0000

    Close HttpWebResponse or we run out of conection after 2 attemps. + Test setup in separate function

--- TorahWayPodcast.Tests/ItunesTest.cs
+++ TorahWayPodcast.Tests/ItunesTest.cs
@@ -16,29 +16,51 @@ namespace TorahWayPodcast.Tests
     [TestFixture]
     public class ItunesTest
     {
-        [Test]
-        public void AreAllUrlsValid()
+        Shiurim Shiurim;
+
+        [TestFixtureSetUp]
+        public void ReadShiurimFromFile()
         {
             IStorage<Shiurim> Storage = new FileStorage(@"E:\GitHub\torahwaypodcast\TorahWayPodcast.Tests\test_data\shiurim.dat");
             Assert.IsNotNull(Storage);
 
-            var shiurim = Storage.Read();
+            Shiurim = Storage.Read();
 
-            Assert.IsNotNull(shiurim);
+            Assert.IsNotNull(Shiurim);
 
             Log("Shiurim");
-            Log("Shiurim has count of "  + shiurim.Count);
-            Assert.IsTrue(shiurim.Count > 0);
+            Log("Shiurim has count of " + Shiurim.Count);
+            Assert.IsTrue(Shiurim.Count > 0);
+        }
 
+        [Test]
+        public void AllUrAreValid()
+        {
             int i = 1;
-            foreach (Shiur shiur in shiurim)
+            foreach (Shiur shiur in Shiurim.OrderBy(x => Guid.NewGuid())) // queryURLs in random order
             {
-                Log(string.Format("Testing URL {0} ({1}/{2})", shiur.Url, i++, shiurim.Count));
-                HttpWebRequest http = (HttpWebRequest)HttpWebRequest.Create(shiur.Url);
-                http.Method = "HEAD";
-                HttpWebResponse response = (HttpWebResponse)http.GetResponse();
+                try
+                {
+                    Log(string.Format("Testing URL {0} ({1}/{2})", shiur.Url, i++, Shiurim.Count));
+                    HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(shiur.Url);
+                    request.Method = "HEAD";
+                    request.Timeout = 5000; // 5s
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+                        //Log(String.Join("\n", response.Headers));
 
-                Assert.AreEqual(response.StatusCode, HttpStatusCode.OK, String.Format("{0} has return code of {1}", shiur.Url, response.StatusCode));
+                        Assert.AreEqual(response.StatusCode, HttpStatusCode.OK, String.Format("{0} has return code of {1}", shiur.Url, response.StatusCode));
+                    }
+                }
+                catch (WebException e)
+                {
+                    if (e.Status == WebExceptionStatus.Timeout)
+                    {
+                        Assert.Fail(shiur.Url + " timed out");
+                    }
+                }
+
+                
             }
 
         }
