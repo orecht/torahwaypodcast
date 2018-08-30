098262069d8761ef91b63ada50407f997fd4cb6a
Oren Recht <orecht.lists@gmail.com>
2018-08-30 01:48:01 +0100

    Simulate AWS lambda funtion run locally using nunit3

---
+++ TorahWayPodcast.AWSLambda.Test/FakeLambdaContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+namespace TorahWayPodcast.AWSLambda.Test
+{
+    public class TestLogger : ILambdaLogger
+    {
+        public void Log(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void LogLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class FakeLambdaContext : ILambdaContext
+    {
+        public string AwsRequestId => throw new NotImplementedException();
+
+        public IClientContext ClientContext => throw new NotImplementedException();
+
+        public string FunctionName => throw new NotImplementedException();
+
+        public string FunctionVersion => throw new NotImplementedException();
+
+        public ICognitoIdentity Identity => throw new NotImplementedException();
+
+        public string InvokedFunctionArn => throw new NotImplementedException();
+
+        public ILambdaLogger Logger => new TestLogger();
+
+        public string LogGroupName => throw new NotImplementedException();
+
+        public string LogStreamName => throw new NotImplementedException();
+
+        public int MemoryLimitInMB => throw new NotImplementedException();
+
+        public TimeSpan RemainingTime => throw new NotImplementedException();
+    }
+}
---
+++ TorahWayPodcast.AWSLambda.Test/LambdaFunction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Amazon.Lambda.Core;
+using NUnit.Framework;
+using TorahWayPodcast.AWSLambda;
+
+namespace TorahWayPodcast.AWSLambda.Test
+{
+    [TestFixture]
+    public class LambdaFunction
+    {
+        [Test]
+        public void RunFunction()
+        {
+            ILambdaContext context = new FakeLambdaContext();
+
+            new Function().FunctionHandler(context);
+        }
+    }
+}
